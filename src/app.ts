1b197f7aa8298dbd2cf3827c9d0a984f2130ea6b
Rykasa <jhonathan-alencar@hotmail.com>
2022-07-03 10:49:38 -0300

    deploy adjustments

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,12 @@ app.use(express.json())
 app.use(cors())
 app.use(router)
 
-const start = async () =>{
-  try{
+const start = async () => {
+  try {
     await connectDB(process.env.MONGO_URI)
 
-    app.listen(4000, () => console.log('Server is running on port 4000...'))
-  }catch(error){
+    app.listen(process.env.PORT || 4000, () => console.log('Server is running on port 4000...'))
+  } catch (error) {
     console.log(error)
   }
 }
